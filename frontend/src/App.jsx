b4353fd62ea9eef8f733832c9f7a0953b8fd46a8
plazadevina <plazadevina@gmail.com>
2025-08-04 15:40:55 +0000

    git commit -m "Code-lab-web"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import { Home } from "./components/Home"
+import { UserInfo } from "./components/UserInfo"
+import { UserSettings } from "./components/components"
 export const App = () => {
 
   return (
     <>
       <h1>State-Management-Zustand-Welcome to Final Project-Special Garden Group!</h1>
+      <Home/>
+      <UserInfo/>
+      <UserSettings/>
     </>
   );
 };
