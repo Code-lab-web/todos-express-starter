d1ee0d2228df4cd2a0b092b7cb594ebc009f6839
your_username <your_email@example.com>
2025-10-10 11:27:00 +0000

    git commit -m "Code-lab-web"

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,4 @@
-module.exports ={
-    presents: [['@babel/present-env', {targets:{node: {node 'current'}}]],
-
-},
-module.exports = api => {
-    const isTest = api.env('test');
-    // You can use isTest to determine what presets and plugins to use.
-  
-    return {
-      // ...
-    };
-  };
-  module.exports = {
+module.exports = {
     presets: [
       ['@babel/preset-env', {targets: {node: 'current'}}],
       '@babel/preset-typescript',
