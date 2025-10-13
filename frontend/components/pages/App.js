31e0232bd13d4aa2e7ad2d3ff53c544a5f2ba971
your_username <your_email@example.com>
2025-10-13 11:07:06 +0000

    git commit -m "Code-lab-web"

--- frontend/components/pages/App.js
+++ frontend/components/pages/App.js
@@ -1,73 +1,47 @@
-import React from 'react'
-import {Welcome} from './Welcome'
-import {About} from './About'
-import {Contact} from './Contact'
-import {Nav} from './Nav'
-export const App = () => {}
-import SGG from './SGG.png';
-import './App.css';
-import GardenCard from "./components/GardenCard"
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Container from '@mui/material/Container';
-import Paper from '@mui/material/Paper';
 import Grid from "@mui/material/Grid";
-import gardens from "../../../pages/data.json"
 import Typography from '@mui/material/Typography';
-import Home from "./pages/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./Home";
+import GardenCard from "./GardenCard";
+import GardenTour from './GardenTour';
+import SearchAppBar from './AppBar';
+import gardens from "./data.json";
+import './App.css';
+
+
 function App() {
     return (
         <BrowserRouter>
-        <SearchAppBar />
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/":/id"  element={<GardenTour />} />
-            
-        </Routes>
-        </BrowserRouter>
-        <div className="App">
             <SearchAppBar />
-            <Container sx={{ marginY: 5 }}></Container>
-            <Container sx={{ margin: 5 }}>
-            {garden.map((garden) => (
-                <Typography
-                variant="h4"
-                component="h2"
-                marginTop={5}
-                marginBottom={3}
-                Top {garden.name} Tours
-                </Typography>
-                />
-            ))>
-
-            ))}
-            )
-                <Grid container spacing={5}>
-                    {garden.tours.map((tour) => <GardenCard tour={tour} key={index}/>/)>})
-                    <GardenCard />
-                    <GardenCard />
-                    <GardenCard />
-                    <GardenCard />
-                </Grid>
-            </Container>
-            <Container>
-                <Grid container spacing={5}>
-                    <GardenCard />
-                </Grid>
-            </Container>
-            <header>
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                    <a
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >
-                        Special Garden Group
-                    </a>
-                </p>
-            </header>
-        </div>
-    )
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/:id"  element={<GardenTour />} />
+            </Routes>
+            <div className="App">
+                <Container sx={{ marginY: 5 }}>
+                    {gardens.map((garden) => (
+                        <div key={garden.id}>
+                            <Typography
+                                variant="h4"
+                                component="h2"
+                                marginTop={5}
+                                marginBottom={3}
+                            >
+                                Top {garden.name} Tours
+                            </Typography>
+                            <Grid container spacing={5}>
+                                {garden.tours.map((tour, index) => (
+                                    <GardenCard tour={tour} key={index} />
+                                ))}
+                            </Grid>
+                        </div>
+                    ))}
+                </Container>
+            </div>
+        </BrowserRouter>
+    );
 }
+
 export default App;
